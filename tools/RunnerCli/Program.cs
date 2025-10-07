d7c95917df063891a86b43c40e205c0da5451127
hutchisonkim <kim@shade.ca>
2025-10-07 14:32:13 -0400

    chore: remove RunnerCli directory and associated project files after migration to GitHub.Runner.Docker.Cli

--- tools/RunnerCli/Program.cs
+++
@@ -1,3 +0,0 @@
-// This directory has been replaced by tools/GitHub.Runner.Docker.Cli
-// The real CLI lives at: tools/GitHub.Runner.Docker.Cli
-// Kept here as a lightweight pointer during the migration.
